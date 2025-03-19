0adedf7fa7f551801b0c39a5d2b07abad074814d
watup <eventionation@gmail.com>
2025-03-19 00:30:02 +0000

    suryapraknixnetv4

--- models/suryapraknixnet.py
+++ models/suryapraknixnet.py
@@ -3,21 +3,32 @@ import torch.nn.functional as F
 import torch
 
 
-class ResNetBlock(nn.Module):
+class BRBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
-        super(ResNetBlock, self).__init__()
-        conv = lambda in_channels, out_channels, kernel_size, stride: nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding=1, bias=False)
-        self.layers = nn.ModuleList([conv(in_channels, out_channels, 3, stride),
-                                     nn.BatchNorm2d(out_channels),
-                                     conv(out_channels, out_channels, 3, 1),
-                                     nn.BatchNorm2d(out_channels)])
+        super(BRBlock, self).__init__()
+        """Best practice is to just list out layers explicitly."""
+        expansion = 4  # how much input/output channels differ by
+        mid_channels = out_channels // expansion
 
+        # Reduce
+        self.conv1 = nn.Conv2d(in_channels, mid_channels, kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn1 = nn.BatchNorm2d(mid_channels)
+        
+        # Process
+        self.conv2 = nn.Conv2d(mid_channels, mid_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(mid_channels)
+
+        # Expand 
+        self.conv3 = nn.Conv2d(mid_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels)
+
+        self.shortcut = nn.Identity()
         if stride != 1 or out_channels != in_channels:
-            self.layers.append(conv(in_channels, out_channels, 3, stride))
-            self.layers.append(nn.BatchNorm2d(out_channels))
-        else:
-            self.layers.append(nn.Sequential())
-
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+        self.activation = nn.ReLU()
         ## END YOUR CODE
 
     def forward(self, x):
@@ -28,23 +39,18 @@ class ResNetBlock(nn.Module):
         returns: result of passing x through this block
         """
         ## YOUR CODE HERE
-        idd = x.clone().detach()
-        first_segment = self.layers[:2]
-        second_segment = self.layers[2:4]
-        shortcut = self.layers[4:]
+        idd = self.shortcut(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.activation(x)
 
-        for layer in first_segment:
-            x = layer(x)
-        x = nn.functional.gelu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
 
-        for layer in second_segment:
-            x = layer(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
 
-        for layer in shortcut:
-            idd = layer(idd)
-        
-        x = nn.functional.gelu(x + idd)
-
+        x = self.activation(x + idd)
         return x
 
 class SuryaPrakNixNet(nn.Module):
@@ -58,26 +64,25 @@ class SuryaPrakNixNet(nn.Module):
         super(SuryaPrakNixNet, self).__init__()
         self.in_channels = 64   
         self.features = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1, bias=False),  
-            nn.BatchNorm2d(32),
-            nn.GELU(),
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1, bias=False),  
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),  
             nn.BatchNorm2d(64),
             nn.GELU(),
             self.make_block(out_channels=64, stride=1, rep=2),
-            self.make_block(out_channels=128, stride=2, rep=2),
-            self.make_block(out_channels=512, stride=2, rep=2),
-            self.make_block(out_channels=512, stride=2, rep=2),
-            self.make_block(out_channels=1024, stride=2, rep=2),
-            self.make_block(out_channels=1024, stride=2, rep=2),
+            self.make_block(out_channels=128, stride=2, rep=3),
+            self.make_block(out_channels=512, stride=2, rep=4),
+            self.make_block(out_channels=1024, stride=2, rep=6),
             nn.AdaptiveAvgPool2d(1)
         )
 
         self.classifer = nn.Sequential(
-            nn.Linear(1024, 512),
+            nn.Linear(1024, 1024),
+            nn.LayerNorm(1024),
+            nn.GELU(),
+            nn.Dropout(0.5),
+            nn.Linear(1024, 52),
             nn.LayerNorm(512),
             nn.GELU(),
-            nn.Dropout(0.2),
+            nn.Dropout(0.5),
             nn.Linear(512, num_classes)
         )
         
@@ -86,7 +91,7 @@ class SuryaPrakNixNet(nn.Module):
     def make_block(self, out_channels, stride, rep):
         layers = []
         for stride in [stride, 1]:
-            layers.append(ResNetBlock(self.in_channels, out_channels, stride))
+            layers.append(BRBlock(self.in_channels, out_channels, stride))
             self.in_channels = out_channels
         return nn.Sequential(*layers)
 
